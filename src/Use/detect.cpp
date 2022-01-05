9cec149a68fd00780de77bde997aa137125158d6
User <prbodmann@gmail.com>
2022-01-05 11:16:56 -0300

    added testbench

---
+++ src/Use/detect.cpp
@@ -0,0 +1,24 @@
+#include "Simd/SimdDetection.hpp"
+#include "Simd/SimdDrawing.hpp"
+#include <iostream>
+int main(int argc, char** argv)
+{
+    typedef Simd::Detection<Simd::Allocator> Detection;
+ 
+    Detection::View image;
+
+    image.Load(argv[2]);//"../../data/image/face/pp.pgm"
+ 
+    Detection detection;
+ 
+    detection.Load(argv[1]);//"../../data/cascade/haar_face_0.xml"
+    detection.Init(image.Size());
+    Detection::Objects objects;
+    detection.Detect(image, objects);    
+    for (size_t i = 0; i < objects.size(); ++i){
+        Simd::DrawRectangle(image, objects[i].rect, uint8_t(255));
+    }
+    image.Save("result.pgm");
+ 
+    return 0;
+}
